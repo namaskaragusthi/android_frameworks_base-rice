wavedashdoc/android_frameworks_base

SystemUI: Ambient Edge Pulse layout [1/2]
- option to choose between faded & solid view

Signed-off-by: spezi77 <spezi7713@gmx.net>

packages/SystemUI/src/com/android/systemui/custom/NotificationLightsView.java
@@ -88,10 +88,21 @@ public class NotificationLightsView extends RelativeLayout {
     }
 
     public void animateNotificationWithColor(int color) {
-        ImageView leftView = (ImageView) findViewById(R.id.notification_animation_left);
-        ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
-        leftView.setColorFilter(color);
-        rightView.setColorFilter(color);
+        int layout = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT_LAYOUT, 0,
+                UserHandle.USER_CURRENT);
+        ImageView leftViewSolid = (ImageView) findViewById(R.id.notification_animation_left_solid);
+        ImageView leftViewFaded = (ImageView) findViewById(R.id.notification_animation_left_faded);
+        leftViewSolid.setColorFilter(color);
+        leftViewFaded.setColorFilter(color);
+        leftViewSolid.setVisibility(layout == 0 ? View.VISIBLE : View.GONE);
+        leftViewFaded.setVisibility(layout == 1 ? View.VISIBLE : View.GONE);
+        ImageView rightViewSolid = (ImageView) findViewById(R.id.notification_animation_right_solid);
+        ImageView rightViewFaded = (ImageView) findViewById(R.id.notification_animation_right_faded);
+        rightViewSolid.setColorFilter(color);
+        rightViewFaded.setColorFilter(color);
+        rightViewSolid.setVisibility(layout == 0 ? View.VISIBLE : View.GONE);
+        rightViewFaded.setVisibility(layout == 1 ? View.VISIBLE : View.GONE);
 
         int duration = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.PULSE_AMBIENT_LIGHT_DURATION, 2,
@@ -104,16 +115,20 @@ public class NotificationLightsView extends RelativeLayout {
             public void onAnimationUpdate(ValueAnimator animation) {
                 if (DEBUG) Log.d(TAG, "onAnimationUpdate");
                 float progress = ((Float) animation.getAnimatedValue()).floatValue();
-                leftView.setScaleY(progress);
-                rightView.setScaleY(progress);
+                leftViewSolid.setScaleY(progress);
+                leftViewFaded.setScaleY(progress);
+                rightViewSolid.setScaleY(progress);
+                rightViewFaded.setScaleY(progress);
                 float alpha = 1.0f;
                 if (progress <= 0.3f) {
                     alpha = progress / 0.3f;
                 } else if (progress >= 1.0f) {
                     alpha = 2.0f - progress;
                 }
-                leftView.setAlpha(alpha);
-                rightView.setAlpha(alpha);
+                leftViewSolid.setAlpha(alpha);
+                leftViewFaded.setAlpha(alpha);
+                rightViewSolid.setAlpha(alpha);
+                rightViewFaded.setAlpha(alpha);
             }
         });
         if (DEBUG) Log.d(TAG, "start");

namaskaragusthi/android_frameworks_base-rice

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/phone/NotificationLightsView.java
@@ -86,30 +86,45 @@ public class NotificationLightsView extends RelativeLayout {
         int duration = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.PULSE_AMBIENT_LIGHT_DURATION, 2,
                 UserHandle.USER_CURRENT) * 1000;
+        int layout = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.PULSE_AMBIENT_LIGHT_LAYOUT, 0,
+                UserHandle.USER_CURRENT);
         StringBuilder sb = new StringBuilder();
         sb.append("animateNotification color ");
         sb.append(Integer.toHexString(color));
         Log.e("NotificationLightsView", sb.toString());
-        ImageView leftView = (ImageView) findViewById(R.id.notification_animation_left);
-        ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
-        leftView.setColorFilter(color);
-        rightView.setColorFilter(color);
+        ImageView leftViewSolid = (ImageView) findViewById(R.id.notification_animation_left_solid);
+        ImageView leftViewFaded = (ImageView) findViewById(R.id.notification_animation_left_faded);
+        leftViewSolid.setColorFilter(color);
+        leftViewFaded.setColorFilter(color);
+        leftViewSolid.setVisibility(layout == 0 ? View.VISIBLE : View.GONE);
+        leftViewFaded.setVisibility(layout == 1 ? View.VISIBLE : View.GONE);
+        ImageView rightViewSolid = (ImageView) findViewById(R.id.notification_animation_right_solid);
+        ImageView rightViewFaded = (ImageView) findViewById(R.id.notification_animation_right_faded);
+        rightViewSolid.setColorFilter(color);
+        rightViewFaded.setColorFilter(color);
+        rightViewSolid.setVisibility(layout == 0 ? View.VISIBLE : View.GONE);
+        rightViewFaded.setVisibility(layout == 1 ? View.VISIBLE : View.GONE);
         mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
         mLightAnimator.setDuration(duration);
         mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
             public void onAnimationUpdate(ValueAnimator animation) {
                 Log.e("NotificationLightsView", "onAnimationUpdate");
                 float progress = ((Float) animation.getAnimatedValue()).floatValue();
-                leftView.setScaleY(progress);
-                rightView.setScaleY(progress);
+                leftViewSolid.setScaleY(progress);
+                leftViewFaded.setScaleY(progress);
+                rightViewSolid.setScaleY(progress);
+                rightViewFaded.setScaleY(progress);
                 float alpha = 1.0f;
                 if (progress <= 0.3f) {
                     alpha = progress / 0.3f;
                 } else if (progress >= 1.0f) {
                     alpha = 2.0f - progress;
                 }
-                leftView.setAlpha(alpha);
-                rightView.setAlpha(alpha);
+                leftViewSolid.setAlpha(alpha);
+                leftViewFaded.setAlpha(alpha);
+                rightViewSolid.setAlpha(alpha);
+                rightViewFaded.setAlpha(alpha);
             }
         });
         Log.e("NotificationLightsView", "start");